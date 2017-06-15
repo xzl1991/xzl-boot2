e9a81d8343981910f940fc38337efd7f32cf674d
xuzelong <xuzelong@gomeholdings.com>
2017-06-15 19:05:17 +0800

    测试session共享

--- src/main/java/example/APP.java
+++ src/main/java/example/APP.java
@@ -1,11 +1,23 @@
 package example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class APP {
+@RestController
+@EnableAutoConfiguration
+@ComponentScan
+public class APP implements EmbeddedServletContainerCustomizer {
 	   public static void main(String[] args) throws Exception {
 	        SpringApplication.run(APP.class, args);
 	    }
+
+	public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
+		configurableEmbeddedServletContainer.setPort(8889);
+	}
 }
