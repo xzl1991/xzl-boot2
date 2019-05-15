9322937e3ff91260aa6f56c2e27cd3bd114d37d7
unknown <1192373005@qq.com>
2019-05-16 01:05:29 +0800

    xml读取

--- src/main/java/com/xzl/controller/TestController.java
+++ src/main/java/com/xzl/controller/TestController.java
@@ -1,19 +1,19 @@
-package com.xzl.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-/**
- * Created by Administrator on 2018/12/17.
- */
-@Controller
-@RequestMapping("/test")
-public class TestController {
-    @ResponseBody
-    @RequestMapping("/getName/{name}")
-    public String getName(@PathVariable String name){
-        return "===="+name;
-    }
-}
+//package com.xzl.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+///**
+// * Created by Administrator on 2018/12/17.
+// */
+//@Controller
+//@RequestMapping("/test")
+//public class TestController {
+//    @ResponseBody
+//    @RequestMapping("/getName/{name}")
+//    public String getName(@PathVariable String name){
+//        return "===="+name;
+//    }
+//}
